18abde926408adfef3f695e8ff5264e7b2eeadec
Nmb1171 <nahombadebo72@gmail.com>
2024-10-05 20:36:24 +0300

    Updates on Display Css in App.jsx, Home.jsx, Services.jsx

--- Imaanify/src/App.jsx
+++ Imaanify/src/App.jsx
@@ -3,12 +3,12 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import AboutUs from "./components/AboutUs";
 import Services from "./components/Services";
 import Contact from "./components/Contact";
-import Home from "./components/Home";
+import Home from "./components/Home"; 
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-100">
+      <div className="w-screen h-screen  bg-gray-100"> 
         <header className="bg-purple-500 p-4 text-black text-center"> {/* Corrected 'text-Black' to 'text-black' */}
           <h1 className="text-3xl font-bold">Imaanify</h1>
           <p className="text-lg">Connecting Muslims Worldwide</p>
--- Imaanify/src/components/Home.jsx
+++ Imaanify/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className="text-center my-8 text-black">
+    <div className=" w-screen h-screen text-center my-8 text-black">
       <h2 className="text-4xl font-bold mb-4 text-black">Welcome to Imaanify</h2>
       <p className="text-xl text-black-700">
         Join our community and connect with Muslims globally.
